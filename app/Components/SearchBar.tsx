0f168b8c0c0122595bb7e25968fa2fcd704095b9
whoknowsi <echosmania@gmail.com>
2023-03-01 16:16:47 -0300

    Change on padding searchbar

--- app/Components/SearchBar.tsx
+++ app/Components/SearchBar.tsx
@@ -2,7 +2,7 @@ import LensIcon from "./Icons/LensIcon"
 
 const SearchBar = () => {
   return (
-    <div className="w-full flex gap-2 items-center rounded-2xl bg-white px-4 py-2"><LensIcon color='#ddd' width={22} height={22}/><input placeholder="Buscá servicios y materiales" className="p-2 w-full" type='search'/></div>
+    <div className="w-full flex gap-2 items-center rounded-2xl bg-white px-4 py-1"><LensIcon color='#ddd' width={22} height={22}/><input placeholder="Buscá servicios y materiales" className="p-2 w-full" type='search'/></div>
   )
 }
 
