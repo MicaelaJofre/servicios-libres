e4102233f122f7e453ec373236147908c56f1d6f
whoknowsi <echosmania@gmail.com>
2023-03-01 15:16:32 -0300

    Added custom gradient on tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'orange-gradient': "linear-gradient(to right bottom, #FF6A76, #FFC37B)",
+      }
+    },
   },
   plugins: [],
 }
