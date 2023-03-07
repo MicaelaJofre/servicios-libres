d8bbc3519cad2d901e9af1a7714c4fd231ca0eff
Micaela Analia Jofre <mkjofre@gmail.com>
2023-03-07 00:38:08 -0300

    We remove double quotation marks - eslint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'orange-gradient': "linear-gradient(to right, #FF6A76, #FFC37B)",
+        'orange-gradient': 'linear-gradient(to right, #FF6A76, #FFC37B)'
       },
       colors: {
-        'orange-light': "#FFC37B",
-        'orange-dark': "#FF6A76"
+        'orange-light': '#FFC37B',
+        'orange-dark': '#FF6A76'
       }
-    },
+    }
   },
-  plugins: [],
+  plugins: []
 }
