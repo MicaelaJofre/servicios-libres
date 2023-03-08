3b8736cdd9d189c5e9e4b9d9c6507b708216d674
whoknowsi <echosmania@gmail.com>
2023-03-08 13:38:34 -0300

    Added icons

---
+++ app/Components/Icons/HearthIcon.tsx
@@ -0,0 +1,15 @@
+import { IconProps } from './IconTypes'
+
+const HearthIcon = ({ color, width, height }: IconProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={width}
+    height={height}
+    viewBox="0 0 24 24"
+    fill={color.firstColor}
+  >
+    <path d="M12 4.435C10.011-.964 0-.162 0 8.003 0 12.071 3.06 17.484 12 23c8.94-5.516 12-10.929 12-14.997C24-.115 14-.996 12 4.435z" />
+  </svg>
+)
+
+export default HearthIcon
