03f0d638d006da344ab452af0f7e9eb4ed0105b0
Micaela Analia Jofre <mkjofre@gmail.com>
2023-03-09 00:01:02 -0300

    implements useContext with isMenuOpen

--- app/Components/Menu/Menu.tsx
+++ app/Components/Menu/Menu.tsx
@@ -1,13 +1,23 @@
+'use client'
+
+import { MenuContext } from '@/app/context/menu'
+import { useContext } from 'react'
 import MenuAvatar from './MenuAvatar'
 import MenuList from './MenuList'
 
 const Menu = () => {
+  const { isMenuOpen } = useContext(MenuContext)
   return (
-    <div className="bg-white w-[350px] h-full absolute z-50 left-0 top-0 p-4 flex flex-col gap-5 pt-8">
-      <MenuAvatar />
-      <hr className="shadow-sm shadow-slate-300/20"/>
-      <MenuList />
-    </div>
+    <>
+    {
+      isMenuOpen &&
+        <div className="bg-white w-[350px] h-full absolute z-50 left-0 top-0 p-4 flex flex-col gap-5 pt-8">
+              <MenuAvatar />
+              <hr className="shadow-sm shadow-slate-300/20"/>
+              <MenuList />
+            </div>
+    }
+    </>
   )
 }
 
