1081878728209f6c9f2b01d53f3fdcc83831c912
Micaela Analia Jofre <mkjofre@gmail.com>
2023-03-09 00:18:48 -0300

    the menu is positioned in relation to the container

--- app/Components/Menu/Menu.tsx
+++ app/Components/Menu/Menu.tsx
@@ -11,7 +11,7 @@ const Menu = () => {
     <>
     {
       isMenuOpen &&
-        <div className="bg-white w-[350px] h-full absolute z-50 left-0 top-0 p-4 flex flex-col gap-5 pt-8">
+        <div className="bg-white w-[350px] h-full absolute z-50 left-0 -top-[115px] p-4 flex flex-col gap-5 pt-8">
               <MenuAvatar />
               <hr className="shadow-sm shadow-slate-300/20"/>
               <MenuList />
